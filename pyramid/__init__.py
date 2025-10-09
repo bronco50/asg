38bb5f3544e225f60e668d9639dcd004bc94eeca
Mr. Lewis <106601886+bronco50@users.noreply.github.com>
2025-10-09 15:31:34 -0500

    Update __init__.py

--- pyramid/__init__.py
+++ pyramid/__init__.py
@@ -12,7 +12,7 @@ def exists():
 def test_integer_values():
     """input of 4, 6, and 9 yields correct formatted output"""
     inputs = ["4", "6", "9"]
-    output = "Enter base length: Enter base width: Enter height: Volume: 72.0"
+    output = "Enter base length: Enter base width: Enter height: Volume: 72.0/n"
     check50.run("python3 pyramid.py")\
         .stdin(inputs[0], prompt=False)\
         .stdin(inputs[1], prompt=False)\
@@ -25,7 +25,7 @@ def test_integer_values():
 def test_float_values():
     """input of 3, 5, and 7.5 yields correct formatted output"""
     inputs = ["3", "5", "7.5"]
-    output = "Enter base length: Enter base width: Enter height: Volume: 37.5"
+    output = "Enter base length: Enter base width: Enter height: Volume: 37.5/n"
     check50.run("python3 pyramid.py")\
         .stdin(inputs[0], prompt=False)\
         .stdin(inputs[1], prompt=False)\
