19a5eb7e3d4aee0d01fcb68c9cf60d14399923c4
Mr. Lewis <106601886+bronco50@users.noreply.github.com>
2025-10-09 13:54:52 -0500

    Update __init__.py

--- pyramid/__init__.py
+++ pyramid/__init__.py
@@ -1,4 +1,5 @@
 import check50
+import re
 
 @check50.check()
 def exists():
@@ -9,31 +10,34 @@ def exists():
 @check50.check(exists)
 def test_volume_integer():
     """input of 4, 6, 9 yields output of 72.00"""
-    check50.run("python3 pyramid.py")\
+    actual = check50.run("python3 pyramid.py")\
         .stdin("4", prompt=False)\
         .stdin("6", prompt=False)\
         .stdin("9", prompt=False)\
-        .stdout("72.00\n", regex=False)\
-        .exit(0)
+        .stdout()
+    if not re.search(r"(^|\s)72\.00(\s|$)", actual):
+        raise check50.Mismatch("72.00\n", actual)
 
 
 @check50.check(exists)
 def test_volume_float():
     """input of 3, 5, 7.5 yields output of 37.50"""
-    check50.run("python3 pyramid.py")\
+    actual = check50.run("python3 pyramid.py")\
         .stdin("3", prompt=False)\
         .stdin("5", prompt=False)\
         .stdin("7.5", prompt=False)\
-        .stdout("37.50\n", regex=False)\
-        .exit(0)
+        .stdout()
+    if not re.search(r"(^|\s)37\.50(\s|$)", actual):
+        raise check50.Mismatch("37.50\n", actual)
 
 
 @check50.check(exists)
 def test_volume_decimal():
     """input of 4.25, 6.75, 9.1 yields output of 87.02"""
-    check50.run("python3 pyramid.py")\
+    actual = check50.run("python3 pyramid.py")\
         .stdin("4.25", prompt=False)\
         .stdin("6.75", prompt=False)\
         .stdin("9.1", prompt=False)\
-        .stdout("87.02\n", regex=False)\
-        .exit(0)
+        .stdout()
+    if not re.search(r"(^|\s)87\.02(\s|$)", actual):
+        raise check50.Mismatch("87.02\n", actual)
