d389f4d95ef974ca35954b8e44f33fa12dc79218
Geno Lewis <106601886+bronco50@users.noreply.github.com>
2025-10-30 11:08:13 -0500

    Update __init__.py

--- password/__init__.py
+++ password/__init__.py
@@ -23,16 +23,16 @@ def test_very_weak_password():
 
 @check50.check(exists)
 def test_weak_password():
-    """'moonbase123' yields Score: 22 and Strength: Weak"""
+    """'moonbase123' yields Score: 12 and Strength: Weak"""
     result = (
         check50.run("python3 password.py")
-        .stdin("moonbase123", prompt=True)
+        .stdin("moonbase", prompt=True)
         .stdout()
         .strip()
     )
-    if "Score: 22" not in result:
+    if "Score: 12" not in result:
         help = "Make sure you are assigning the points correctly."
-        raise check50.Mismatch("Score: 22", result, help=help)
+        raise check50.Mismatch("Score: 12", result, help=help)
     if "Strength: Weak" not in result:
         help = "Make sure you are providing the appropriate rating."
         raise check50.Mismatch("Strength: Weak", result, help=help)
@@ -40,16 +40,16 @@ def test_weak_password():
 
 @check50.check(exists)
 def test_moderate_password():
-    """'Mars!2025' yields Score: 30 and Strength: Moderate"""
+    """'Mars!2025' yields Score: 25 and Strength: Moderate"""
     result = (
         check50.run("python3 password.py")
         .stdin("Mars!2025", prompt=True)
         .stdout()
         .strip()
     )
-    if "Score: 30" not in result:
+    if "Score: 25" not in result:
         help = "Make sure you are assigning the points correctly."
-        raise check50.Mismatch("Score: 30", result, help=help)
+        raise check50.Mismatch("Score: 25", result, help=help)
     if "Strength: Moderate" not in result:
         help = "Make sure you are providing the appropriate rating."
         raise check50.Mismatch("Strength: Moderate", result, help=help)
@@ -57,16 +57,16 @@ def test_moderate_password():
 
 @check50.check(exists)
 def test_strong_password():
-    """'LaunchCode2025' yields Score: 38 and Strength: Strong"""
+    """'LaunchCode2025' yields Score: 31 and Strength: Strong"""
     result = (
         check50.run("python3 password.py")
         .stdin("LaunchCode2025", prompt=True)
         .stdout()
         .strip()
     )
-    if "Score: 38" not in result:
+    if "Score: 31" not in result:
         help = "Make sure you are assigning the points correctly."
-        raise check50.Mismatch("Score: 38", result, help=help)
+        raise check50.Mismatch("Score: 31", result, help=help)
     if "Strength: Strong" not in result:
         help = "Make sure you are providing the appropriate rating."
         raise check50.Mismatch("Strength: Strong", result, help=help)
